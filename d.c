d7c47dec9941ba96f77d8541d1ed92b6c8d68b7a
kirithicute <kirithibaskar@gmail.com>
2016-09-17 16:22:52 +0530

    Create d.c

---
+++ d.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int ar[5],i,j,sum,min,max;
+for(i=0;i<=5;i++)
+{
+scanf("%d",&ar[i]);
+}
+min=a[0];max=ar[5];
+for(i=1;i<=5;i++)
+{
+if(min>ar[i])
+{
+min=ar[i];
+}
+}
+for(j=5;j<=0;j--)
+{
+if(max<ar[i])
+{
+max=ar[i];
+}
+}
+sum=min+max;
+printf("%d",sum);
+}
