3ffb43bbc8128b1acc28542fa627637819d11b62
kirithicute <kirithibaskar@gmail.com>
2016-09-17 16:21:58 +0530

    Create y.c

---
+++ y.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int aa[5],i,j,sum,min,max;
+for(i=0;i<=5;i++)
+{
+scanf("%d",&aa[i]);
+}
+min=a[0];max=aa[5];
+for(i=1;i<=5;i++)
+{
+if(min>aa[i])
+{
+min=aa[i];
+}
+}
+for(j=5;j<=0;j--)
+{
+if(max<aa[i])
+{
+max=aa[i];
+}
+}
+sum=min+max;
+printf("%d",sum);
+}
